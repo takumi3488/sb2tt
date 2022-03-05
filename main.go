1796b8b5d0f026d5d90c8646710d91a20fbbcfc1
Takumi Mori <51111242+tak3488@users.noreply.github.com>
2022-03-06 00:04:43 +0900

    LINE Bot 接続確認

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 		println("OK")
 	}
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.POST("/line", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "OK",
 		})
 	})
 	r.Run()
